827e2ff20289b3154d63c9400922ccf136d7cd0b
Sheol <tf.sheol@gmail.com>
2018-04-03 23:07:48 +0200

    update

--- root/init.sh
+++ root/init.sh
@@ -4,3 +4,8 @@ export JAVA_HOME="/media/java/jvm/default-jvm" \
 MAVEN_HOME="/media/maven/home" \
 PATH="/media/maven/home/bin:$JAVA_HOME/bin:$PATH"
 
+ln -s "$JAVA_HOME/bin/"* "/usr/bin/" && \
+ln -s "/media/maven/home/maven/bin/mvn" "/usr/bin/mvn"
+
+/scripts/glibc-install.sh
+
